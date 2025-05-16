86b56d2efe72a9215200127fe5c3dbbfc8b89b34
unknown <laptop@school.com>
2025-05-16 08:48:09 +0800

    Update Quartz content

--- quartz.config.ts
+++ quartz.config.ts
@@ -93,7 +93,7 @@ const config: QuartzConfig = {
       Plugin.Static(),
       Plugin.Favicon(),
       Plugin.NotFoundPage(),
-      Plugin.CustomOgImages(),
+      // Plugin.CustomOgImages(),
     ],
   },
 }
